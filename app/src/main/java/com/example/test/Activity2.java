fe5ed254c4b8912aa053bd25261378ecc54bca8b
Aya Al-Olabi <angel4249@Ayas-MacBook-Pro.local>
2022-02-21 12:58:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/test/Activity2.java
@@ -0,0 +1,35 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+
+
+
+
+        final Button act3_2= (Button) findViewById(R.id.button);
+
+
+
+
+
+
+
+    }
+}
